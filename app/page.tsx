964d9ec1db80eaaefcbfe051f9fcb363f11bba3c
Gerald <geraldbaalham@live.co.uk>
2025-03-24 12:16:44 +0000

    2:10 landing page complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+import BgGradient from "@/components/common/bg-gradient";
+import CTASection from "@/components/home/cta-section";
+import DemoSection from "@/components/home/demo-section";
+import HeroSection from "@/components/home/hero-section";
+import HowItWorksSection from "@/components/home/how-it-works-section";
+import PricingSection from "@/components/home/pricing-section";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="relative w-full">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+        <DemoSection />
+        <HowItWorksSection />
+        <PricingSection />
+        <CTASection />
+      </div>
+    </div>
+  );
 }
