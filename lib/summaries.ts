f433f52269ababb666a7c2588913eb1ab61c3665
Gerald <geraldbaalham@live.co.uk>
2025-03-27 13:09:45 +0000

    6:00 card with sections, titles and points working, now progress bars

--- lib/summaries.ts
+++ lib/summaries.ts
@@ -6,3 +6,17 @@ export async function getSummaries(userId: string) {
     await sql`SELECT * from pdf_summaries where user_id = ${userId} ORDER BY created_at DESC`;
   return summaries;
 }
+
+export async function getSummaryById(id: string) {
+  try {
+    const sql = await getDbConnection();
+
+    const [summary] =
+      await sql`SELECT id, user_id, title, original_file_url, summary_text, created_at, updated_at, status, file_name, LENGTH(summary_text) - LENGTH(REPLACE (summary_text, ' ', '')) + 1 as word_count FROM pdf_summaries where id=${id}`;
+
+    return summary;
+  } catch (err) {
+    console.error("Error fetching summary by ID ", err);
+    return null;
+  }
+}
